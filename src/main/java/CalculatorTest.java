d2779f9477e6d3e56b4377d6cfc2b8b59e727987
samuelhatar <samuel.hatar@gmail.com>
2025-11-28 18:17:37 +0100

    second init commit

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -3,6 +3,11 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CalculatorTest {
+
+    int firNumber;
+    int secNumber;
+    int theResult;
+
     @Given("The user has enter two numbers {int} and {int}")
     public void theUserHasEnterTwoNumbersAnd(int firstNumber, int secondNumber) {
     }
