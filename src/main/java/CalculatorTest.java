99c98f8d31b1c8960f13641af3856d2c0dda74da
samuelhatar <samuel.hatar@gmail.com>
2025-11-28 17:22:17 +0100

    second init commit

---
+++ src/main/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CalculatorTest {
+    @Given("The user has enter two numbers {int} and {int}")
+    public void theUserHasEnterTwoNumbersAnd(int firstNumber, int secondNumber) {
+    }
+
+    @When("The user sum those two numbers")
+    public void theUserSumThoseTwoNumbers() {
+    }
+
+    @Then("The user can see the result {int}")
+    public void theUserCanSeeTheResult(int expectedResult) {
+    }
+}
--- src/main/resources/pokus.feature
+++ src/main/resources/pokus.feature
@@ -14,3 +14,8 @@ Feature: pokus
       Given nastavenie pociatocnych podmienok
       When Toto je dalsia akcia
       Then Overenie vysledku
+
+      Scenario: Calculator - the sum of two numbers
+        Given The user has enter two numbers 5 and 8
+        When The user sum those two numbers
+        Then The user can see the result 13
